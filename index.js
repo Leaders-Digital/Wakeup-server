8a57dae9e514aad22c73a06dc8f86518c9bdc2cc
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-09-12 12:12:29 +0200

    files created

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const port = 3000; 
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+// server listening
+app.listen(port,()=>{
+    console.log("Our server is running on port 3000");
+})
