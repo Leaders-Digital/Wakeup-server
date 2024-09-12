c091959877f30d357a3139bfa4ab1cc7a5df9f46
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-09-12 16:56:52 +0200

    updates info

--- Models/info.model.js
+++ Models/info.model.js
@@ -4,6 +4,11 @@ const infoDataSchema = new mongoose.Schema({
   email: { type: String, required: true },
   fixNumber: { type: String },
   gsm: { type: String },
+  whatsapp :{type:String} ,
+  adresse : {type:String}, 
+  facebook : {type:String}, 
+  instagram: {type: String}, 
+  tiktok : {type:String},
 });
 
 const infoData = mongoose.model("info", infoDataSchema);
