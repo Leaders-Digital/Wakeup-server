be9d3c4663fc7d379947fb5e8b725ad2ebef6a78
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-09-13 11:34:33 +0200

    products controllers

--- Models/Produit.model.js
+++ Models/Produit.model.js
@@ -1,18 +1,23 @@
 const mongoose = require("mongoose");
+
+// Define the schema for a single variant
+const variantSchema = new mongoose.Schema({
+  quantity: { type: Number },
+  picture: { type: String },
+  color: { type: String },
+  icon: { type: String },
+  reference: { type: String, required: true },
+  codeAbarre: { type: String, unique: true },
+});
+
+// Define the schema for a product
 const productSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   description: { type: String, required: true },
   prix: { type: Number, required: true },
   solde: { type: Boolean, default: false },
   soldePourcentage: { type: Number },
-  varient: {
-    quantity: { type: Number },
-    picture: { type: String },
-    color: { type: String },
-    icon: { type: String },
-    reference: { type: String, required: true },
-    codeAbarre: { type: String, unique: true },
-  },
+  variants: [variantSchema], // Array of variants
 });
 
 const Product = mongoose.model("Product", productSchema);
