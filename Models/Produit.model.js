c170dbda05878a848fb192566443b5838827f0af
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-09-13 17:46:06 +0200

    products

--- Models/Produit.model.js
+++ Models/Produit.model.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const mongoose = require("mongoose");
 
 // Define the schema for a single variant
@@ -16,8 +17,14 @@ const productSchema = new mongoose.Schema({
   description: { type: String, required: true },
   prix: { type: Number, required: true },
   solde: { type: Boolean, default: false },
+  mainPicture: { type: String },
   soldePourcentage: { type: Number },
   variants: [variantSchema], // Array of variants
+  categorie: { 
+    type: String, 
+    enum: ['FACE', 'Brush', 'EYES', 'FACE AND BODY', 'BODY', 'HAIR', 'LIPS'], 
+    required: true 
+  }, // Add categorie enum
 });
 
 const Product = mongoose.model("Product", productSchema);
