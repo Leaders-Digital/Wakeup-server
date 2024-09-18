2a53dd7baa9491598fd85ca5d908f038f4821620
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-09-18 09:40:06 +0200

    updates

--- Routers/Partenaire.router.js
+++ Routers/Partenaire.router.js
@@ -2,12 +2,12 @@ const express = require('express');
 const PartenaireRouter = express.Router();
 const {addPartenaire,getPartenaires}=require('../Controllers/Partenaire.controller');
 const { uploadFile } = require("../Middleware/imageUpload"); // Adjust path
-const upload = require("../Middleware/upload");
+// const upload = require("../Middleware/upload");
 PartenaireRouter.post('/addPartenaire', 
     uploadFile({
-    folder: "./uploads/partenaire",
+    folder: "./uploads",
     acceptedTypes: [".png", ".jpeg", ".jpg"],
-    fieldName: "mainPicture", // This should match the form field name
+    fieldName: "logo", // This should match the form field name
     multiple: false,
   }),addPartenaire);
 PartenaireRouter.get('/getPartenaires',getPartenaires);
