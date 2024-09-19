870a20de2f8e96bedf779a677350f6bdace7349a
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-09-19 15:05:46 +0200

    making updates of schema

--- Models/orders.model.js
+++ Models/orders.model.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
 
-const ordersSchema = new mongoose.Schema({
-  nomPrenom: { type: String, required: true },
-  email: { type: String, required: true },
-  numTelephone: { type: String, required: true },
-  listeDesProduits: [
-    {
-      prodcut: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-      quantite: { type: Number, required: true },
-    },
-  ], // Referencing products
-  adresse: { type: String, required: true },
-  prixTotal: { type: Number, required: true },
-}, { timestamps: true });
+const ordersSchema = new mongoose.Schema(
+  {
+    nom: { type: String, required: true },
+    prenom: { type: String, required: true },
+    email: { type: String, required: true },
+    numTelephone: { type: String, required: true },
+    listeDesProduits: [
+      {
+        variant: { type: mongoose.Schema.Types.ObjectId, ref: "Variant" },
+        quantite: { type: Number, required: true },
+      },
+    ], // Referencing products
+    adresse: { type: String, required: true },
+    gouvernorat: { type: String, required: true },
+    ville: { type: String, required: true },
+    codePostal: { type: String, required: true },
+    note: { type: String },
+    prixTotal: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
 
 const Order = mongoose.model("Order", ordersSchema);
 module.exports = Order;
