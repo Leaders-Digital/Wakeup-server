932693e6c8c5d4064bba117d4f1e89d3393e93a6
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-09-30 16:30:32 +0200

    update status

--- Routers/Reclamation.router.js
+++ Routers/Reclamation.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createReclamation, getReclamation } = require('../Controllers/Reclamation.controller');
+const { createReclamation, getReclamation , updateReclamationStatus } = require('../Controllers/Reclamation.controller');
 
 const reclamationRouter = express.Router();
 
 reclamationRouter.post('/create', createReclamation);
 reclamationRouter.get('/get', getReclamation);
+reclamationRouter.put('/update/:id', updateReclamationStatus);
 
 module.exports = reclamationRouter;
