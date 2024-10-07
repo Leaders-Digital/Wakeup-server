30c2d4dac6838eb929edc94f3ee14f53c7d1bc0f
MedMejri1 <mejrief@gmail.com>
2024-10-07 14:52:34 +0100

    done

--- Routers/subscribe.router.js
+++ Routers/subscribe.router.js
@@ -1,8 +1,9 @@
 // routes/subscriptionRoutes.js
 const express = require('express');
 const routerSub = express.Router();
-const { subscribeEmail } = require('../Controllers/subscribe.controller');
+const { subscribeEmail, getAllSubscriptions } = require('../Controllers/subscribe.controller');
 
 routerSub.post('/', subscribeEmail);
+routerSub.get('/getAllSubscriptions',getAllSubscriptions);
 
 module.exports = routerSub;
