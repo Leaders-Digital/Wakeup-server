e73ff1459ebd07c9b2dfe22e0b0e45c1644f3d3b
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-10-07 09:51:58 +0200

    banners

---
+++ Routers/banner.router.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bannerController = require("../Controllers/banner.controller");
+const bannerRouter = express.Router();
+const { uploadFile } = require("../Middleware/imageUpload"); // Adjust path
+// Create a new banner
+bannerRouter.post(
+  "/create",
+  uploadFile({
+    folder: "./uploads/banners",
+    acceptedTypes: [".png", ".jpeg", ".jpg"],
+    fieldName: "picture", // This should match the form field name
+    multiple: false,
+  }),
+  bannerController.createBanner
+);
+bannerRouter.get("/get", bannerController.getAllBanners);
+// Update a banner by ID
+bannerRouter.put("/:id",
+  uploadFile({
+    folder: "./uploads/banners",
+    acceptedTypes: [".png", ".jpeg", ".jpg"],
+    fieldName: "picture", // This should match the form field name
+    multiple: false,
+  }), bannerController.updateBanner);
+
+// Delete a banner by ID
+bannerRouter.delete("/:id", bannerController.deleteBanner);
+
+module.exports = bannerRouter;
