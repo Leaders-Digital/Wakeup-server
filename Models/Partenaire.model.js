5f347b7000d956ab35c57679605bc68e73cc72dd
khalil <mohamedkhalil.kouroghli@gmail.com>
2024-10-30 14:54:11 +0100

    updates

--- Models/Partenaire.model.js
+++ Models/Partenaire.model.js
@@ -5,6 +5,8 @@ const partenaireSchema = new mongoose.Schema({
     nom: { type: String, required: true },
     logo: { type: String },
     lien: { type: String, required: true },
+    adresse:{ type: String , required: true},
+    telephone:{ type: String , required: true},
     status: { type: Boolean, default: false }
 }, { timestamps: true });
 
