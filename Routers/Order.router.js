c7a77f419a401145fa780f254f434671196a9697
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-11-05 09:59:45 +0100

    payment

--- Routers/Order.router.js
+++ Routers/Order.router.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 
-const OrderRouter =  express.Router();
+const OrderRouter = express.Router();
 
-const {createOrder,getOrders, getOrderById, updateOrderStatus, getOrderByCode , getDeliveredOrders} = require('../Controllers/Order.controller');
+const {
+  createOrder,
+  getOrders,
+  getOrderById,
+  updateOrderStatus,
+  getOrderByCode,
+  getDeliveredOrders,
+  updateOrderPayed,
+  updateOrderPaymentRef,
+} = require("../Controllers/Order.controller");
 
-
-OrderRouter.post('/create',createOrder);
-OrderRouter.get('/',getOrders);
-OrderRouter.get('/livre',getDeliveredOrders);
-OrderRouter.get('/code/:orderCode',getOrderByCode);
-
-OrderRouter.get("/:id",getOrderById);
-OrderRouter.patch("/:id/status",updateOrderStatus);
-
-
+OrderRouter.post("/create", createOrder);
+OrderRouter.get("/", getOrders);
+OrderRouter.get("/livre", getDeliveredOrders);
+OrderRouter.get("/code/:orderCode", getOrderByCode);
+OrderRouter.get("/:id", getOrderById);
+OrderRouter.patch("/:id/status", updateOrderStatus);
+OrderRouter.put("/checkpayment/:id", updateOrderPayed);
+OrderRouter.put("/add-payref/:id", updateOrderPaymentRef);
 
 module.exports = OrderRouter;
