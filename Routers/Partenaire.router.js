6ea11e02d3aa3be9afc81b7c33abf6fa9241a717
MedMejri1 <mejrief@gmail.com>
2024-11-08 10:01:13 +0100

    done

--- Routers/Partenaire.router.js
+++ Routers/Partenaire.router.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const PartenaireRouter = express.Router();
-const {addPartenaire,getPartenaires , deletePartenaire , updatePartenaire}=require('../Controllers/Partenaire.controller');
+const { addPartenaire, getPartenaires, deletePartenaire, updatePartenaire } = require('../Controllers/Partenaire.controller');
 const { uploadFile } = require("../Middleware/imageUpload"); // Adjust path
 // const upload = require("../Middleware/upload");
-PartenaireRouter.post('/addPartenaire', 
-    uploadFile({
+PartenaireRouter.post('/addPartenaire',
+  uploadFile({
     folder: "./uploads",
     acceptedTypes: [".png", ".jpeg", ".jpg"],
     fieldName: "logo", // This should match the form field name
     multiple: false,
-  }),addPartenaire);
-PartenaireRouter.get('/getPartenaires',getPartenaires);
-PartenaireRouter.delete('/deletePartenaire/:id',deletePartenaire);
-PartenaireRouter.put('/updatePartenaire/:id',    uploadFile({
+  }), addPartenaire);
+PartenaireRouter.get('/getPartenaires', getPartenaires);
+PartenaireRouter.delete('/deletePartenaire/:id', deletePartenaire);
+PartenaireRouter.put('/updatePartenaire/:id', uploadFile({
   folder: "./uploads",
   acceptedTypes: [".png", ".jpeg", ".jpg"],
   fieldName: "logo", // This should match the form field name
   multiple: false,
-}),updatePartenaire);
+}), updatePartenaire);
 
 module.exports = PartenaireRouter;
