8724903a92ca2a8b62abad31b49576451eda6bb0
punk010 <mohamed.amine.atouii@gmail.com>
2024-12-24 10:50:02 +0100

    changes

--- Routers/Partenaire.router.js
+++ Routers/Partenaire.router.js
@@ -3,13 +3,19 @@ const PartenaireRouter = express.Router();
 const { addPartenaire, getPartenaires, deletePartenaire, updatePartenaire } = require('../Controllers/Partenaire.controller');
 const { uploadFile } = require("../Middleware/imageUpload"); // Adjust path
 // const upload = require("../Middleware/upload");
-PartenaireRouter.post('/addPartenaire',
-  uploadFile({
-    folder: "./uploads",
-    acceptedTypes: [".png", ".jpeg", ".jpg"],
-    fieldName: "logo", // This should match the form field name
-    multiple: false,
-  }), addPartenaire);
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+PartenaireRouter.post('/addPartenaire', upload.single('logo'), addPartenaire);
 PartenaireRouter.get('/getPartenaires', getPartenaires);
 PartenaireRouter.delete('/deletePartenaire/:id', deletePartenaire);
 PartenaireRouter.put('/updatePartenaire/:id', uploadFile({
